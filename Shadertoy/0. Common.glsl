9d048e6d9d9ca080d0636d5e4ec214c06209c795
rito15 <42164422+rito15@users.noreply.github.com>
2020-11-14 00:02:26 +0900

    Update 0. Common.glsl

--- Shadertoy/0. Common.glsl
+++ Shadertoy/0. Common.glsl
@@ -18,12 +18,35 @@
 #define lerp(a,b,t) mix(a,b,t)
 
 /**************************************************************************************************
- * DEFINITIONS
+ * DEFINITIONS - Constant
+ **************************************************************************************************/
+#define PI 3.141592653589793
+
+/**************************************************************************************************
+ * DEFINITIONS - Function
  **************************************************************************************************/
 #define S(a, b, t) smoothstep(a, b, t)
 
-#define PI 3.141592653589793
+// Remap : (0 ~ 1) => (a, b)
+#define R01(t, a, b) (b - a) * t + a
 
+// Remap : (-1 ~ 1) => (a, b)
+#define R11(t, a, b) (b - a) * (1. - t) * 0.5 + a
+
+/**************************************************************************************************
+ * Functions
+ **************************************************************************************************/
+// UV를 정사각형으로 맞추기
+float2 GetSquareUV(float3 iResolution, float2 fragCoord)
+{
+ 	float ratioWH = iResolution.x / iResolution.y;
+    float ratioHW = iResolution.y / iResolution.x;
+    float2 adjMul = float2(ratioWH, 1.);
+    float2 adjSub = float2((1.-ratioHW) * 0.5 * ratioWH, 0.);
+    float2 uv = (fragCoord/iResolution.xy) * adjMul - adjSub;   
+    
+    return uv; 
+}
 
 /**************************************************************************************************
  * DEBUG Functions 
@@ -40,6 +63,8 @@ float3 debugOutLine(float2 uv)
 }
 float3 debugGrid(float2 uv, float interval)
 {
+    if(interval <= 0.0) return float3(0.);
+    
     // interval마다 그리드 디버그
     float grid = 0.;
     float gridZ = 0.;
